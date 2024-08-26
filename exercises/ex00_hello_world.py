d70ca9862f25dab60158fc141fbc1af3e2639fb8
Ivy Nangalia <ivyn@unc.edu>
2024-08-25 23:50:27 -0400

    name = main FAIL

--- exercises/ex00_hello_world.py
+++ exercises/ex00_hello_world.py
@@ -4,3 +4,7 @@ print("Hello, world!")
 def greet(name: str) -> str:
     """A welcoming first function definition!"""
     return "Hello, " + name + "!"
+
+
+if __name__ == "__main__":
+    greet(name="John")
