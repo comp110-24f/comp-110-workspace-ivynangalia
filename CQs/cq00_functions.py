e3a8b1daee6592b166094fad39661f7c3fcafc6a
Ivy Nangalia <ivyn@unc.edu>
2024-08-28 13:02:34 -0400

    finished cq00

---
+++ CQs/cq00_functions.py
@@ -0,0 +1,17 @@
+"""Challenge question zero!!"""
+
+__author__ = "730670491"
+
+
+def mimic(message: str) -> str:
+    """Returns a message back to you!"""
+    return message
+
+
+def main() -> None:
+    """Main function."""
+    print(mimic(message=input("What is your message?")))
+
+
+if __name__ == "__main__":
+    main()
