92bad8a34ed50688ed4c3dff02ae7173f4259554
Ivy Nangalia <ivyn@unc.edu>
2024-09-09 16:19:17 -0400

    EX01 FOR AUTOGRADER

--- exercises/ex01_tea_party.py
+++ exercises/ex01_tea_party.py
@@ -10,17 +10,23 @@ def tea_bags(people: int) -> int:
 
 def treats(people: int) -> int:
     """How many treats?"""
-    return int(1.5 * tea_bags(people))  # convert back to int since 1.5 returns a float
+    return int(
+        1.5 * tea_bags(people=people)
+    )  # convert back to int since 1.5 returns a float
 
 
-def cost(treat_count: int, tea_count: int) -> float:
+def cost(tea_count: int, treat_count: int) -> float:
     """How much do I owe ya?"""
-    return treat_count * 0.75 + tea_count * 0.5  # multiply then add
+    return (treat_count * 0.75) + (tea_count * 0.5)  # multiply then add
 
 
-def main_planner(guests: int):
+def main_planner(guests: int) -> None:
     """Plan a party!"""
     print(f"A Cozy Tea Party for {guests} People!")  # f strings
-    print(f"Tea Bags: {tea_bags(guests)}")
+    print(f"Tea Bags: {tea_bags(guests)}")  # call function for x amount of guests
     print(f"Treats: {treats(guests)}")
-    print(f"Cost: {cost(treats(guests), tea_bags(guests))}")
+    print(f"Cost: ${cost(tea_bags(guests), treats(guests))}")
+
+
+if __name__ == "__main__":
+    main_planner(guests=int(input("How many guests are attending your tea party? ")))
